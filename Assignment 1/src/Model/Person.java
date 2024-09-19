e7b346f2ba9e91f1bdf4213339c5c07aa0c095ec
Pragna Balaji Rao <balajirao.p@northeastern.edu>
2024-09-18 23:55:40 -0400

    Person Profile

---
+++ Assignment 1/src/Model/Person.java
@@ -0,0 +1,207 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+
+
+/**
+ *
+ * @author Pragna Balaji Rao
+ */
+public class Person { 
+    String FirstName;   
+    String LastName; 
+    String DateofBirth; 
+    String Gender; 
+    String Age;
+    String EducationalLevel; 
+    String Occupation;
+    String EmailID;
+    String PhoneNumber; 
+    String EmergencyContactNumber;
+    String DrriverLicenseNumber;
+    String SocialSecurityNumber;
+    String LicenseState;
+    String City; 
+    String State; 
+    String Country; 
+    String Nationality;
+    String AddressLine1;
+    String AddressLine2;
+    String ZIP;
+    String FaxNumber;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getDateofBirth() {
+        return DateofBirth;
+    }
+
+    public void setDateofBirth(String DateofBirth) {
+        this.DateofBirth = DateofBirth;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getEducationalLevel() {
+        return EducationalLevel;
+    }
+
+    public void setEducationalLevel(String EducationalLevel) {
+        this.EducationalLevel = EducationalLevel;
+    }
+
+    public String getOccupation() {
+        return Occupation;
+    }
+
+    public void setOccupation(String Occupation) {
+        this.Occupation = Occupation;
+    }
+
+    public String getEmailID() {
+        return EmailID;
+    }
+
+    public void setEmailID(String EmailID) {
+        this.EmailID = EmailID;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public String getEmergencyContactNumber() {
+        return EmergencyContactNumber;
+    }
+
+    public void setEmergencyContactNumber(String EmergencyContactNumber) {
+        this.EmergencyContactNumber = EmergencyContactNumber;
+    }
+
+    public String getDrriverLicenseNumber() {
+        return DrriverLicenseNumber;
+    }
+
+    public void setDrriverLicenseNumber(String DrriverLicenseNumber) {
+        this.DrriverLicenseNumber = DrriverLicenseNumber;
+    }
+
+    public String getSocialSecurityNumber() {
+        return SocialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String SocialSecurityNumber) {
+        this.SocialSecurityNumber = SocialSecurityNumber;
+    }
+
+    public String getLicenseState() {
+        return LicenseState;
+    }
+
+    public void setLicenseState(String LicenseState) {
+        this.LicenseState = LicenseState;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public String getNationality() {
+        return Nationality;
+    }
+
+    public void setNationality(String Nationality) {
+        this.Nationality = Nationality;
+    }
+
+    public String getAddressLine1() {
+        return AddressLine1;
+    }
+
+    public void setAddressLine1(String AddressLine1) {
+        this.AddressLine1 = AddressLine1;
+    }
+
+    public String getAddressLine2() {
+        return AddressLine2;
+    }
+
+    public void setAddressLine2(String AddressLine2) {
+        this.AddressLine2 = AddressLine2;
+    }
+
+    public String getZIP() {
+        return ZIP;
+    }
+
+    public void setZIP(String ZIP) {
+        this.ZIP = ZIP;
+    }
+
+    public String getFaxNumber() {
+        return FaxNumber;
+    }
+
+    public void setFaxNumber(String FaxNumber) {
+        this.FaxNumber = FaxNumber;
+    }
+
+    
+    
+    
+}
